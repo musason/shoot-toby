0a4a237cb0a2e65c604f0e584d342a0afb3a5d93
Zelimhan Ismailov <zelim-han@hotmail.com>
2021-01-27 15:05:13 +0100

    3rd commit

--- script.js
+++ script.js
@@ -17,15 +17,15 @@ document.addEventListener("keydown", (event) => {
   if (event.keyCode == 39 || event.keyCode == "ArrowRight") {
     isRightArrow = true;
     isLeftArrow = false;
-    console.log(key);
+
   } else if (event.keyCode == 37 || event.keyCode == "ArrowLeft") {
     isRightArrow = false;
     isLeftArrow = true;
-    console.log(key);
+  
   }
   if (event.keyCode == 32 || event.keyCode == "Space") {
     isSpaceKey = true;
-    console.log(key);
+  
   }
 });
 
@@ -36,7 +36,6 @@ document.addEventListener("keydown", (event) => {
 document.addEventListener("keyup", (event) => {
   isRightArrow = false;
   isLeftArrow = false;
-  isSpaceKey = false;
 });
 
 let backImg = document.createElement("img");
@@ -54,62 +53,134 @@ toby50.src = "images/toby50.png";
 let ball = document.createElement("img");
 ball.src = "images/ball2.png";
 
-function rndWidth() {
-  let rw = Math.floor(Math.random() * 80) + 20;
-  return rw;
-}
-
-// function rndHeight() {
-//   let rh = Math.floor(Math.random() * 100) + 20;
-//   return rh
-// }
-
-let arrToby = [{ x: 30, y: 30, width: rndWidth(), height: rndWidth() }];
-
 let ballY = canvas.height - mike.height - 38;
 let mikeX = (backImg.width - mike.width) / 2;
+let ballX = mikeX + 12;
+let mikeY = backImg.height - mike.height;
 
-let arrBall = [{ x: mikeX + 12, y: ballY }];
+function rndSize() {
+  let rw = Math.floor(Math.random() * 80) + 20;
+  let rh = rw * 1.4
+  return [rw, rh]
+}
+
+let firstSize = rndSize();
+
+function ballPos() {
+  let x = mike + 12
+  let y = ballY
+  return [x,y]
+}
+
+let firstPos = ballPos()
+
+let arrToby = [{ x: 30, y: 30, width: firstSize[0], height: firstSize[1] }];
+
+let balls = [{x:firstPos[0], y: firstPos[1]}]
 
 
 
-function draw() {
-  ctx.drawImage(backImg, 0, 0);
-  ctx.drawImage(mike, mikeX, backImg.height - mike.height);
+
+function drawBall() {
+  if (isSpaceKey) {
+    for (let i = 0; i < balls.length; i++) {
+      ctx.drawImage(ball, balls[i].x, balls[i].y);
+      mikeY -= incrBall;
+      balls.push({ x: firstPos[0], y: -mikeY});
+    }
+  }
+}
+
+
+function drawToby() {
   for (let i = 0; i < arrToby.length; i++) {
     ctx.drawImage(
       toby50,
       arrToby[i].x,
       arrToby[i].y,
       arrToby[i].width,
-      arrToby[i].width
+      arrToby[i].height
     );
     arrToby[i].y++;
+    if (arrToby[i].y == canvas.height+2) {
+      arrToby.shift();
+    }
     if (arrToby[i].y == toby.height / 2) {
+      firstSize = rndSize();
       arrToby.push({
         x: Math.floor(Math.random() * (canvas.width - toby.width)),
         y: -toby.height,
-        width: rndWidth(),
-        height: rndWidth(),
+        width: firstSize[0],
+        height: firstSize[1],
       });
     }
   }
+}
+
+function collision() {
+  for (let i = 0; i < arrToby.length; i++){
+    if (
+    ((mikeX < arrToby[i].x + arrToby[i].width-5 &&
+    mikeX + mike.width+5 > arrToby[i].x)|| (mikeX > arrToby[i].x-5 && mikeX < arrToby[i].x + arrToby[i].width-5)) &&
+    mikeY < arrToby[i].y + arrToby[i].height-5
+  ) {
+    clearInterval(intervalID);
+    alert("GAME OVER");
+    location.reload();
+  }
+  }
+}
+
+function moveMike() {
   if (isRightArrow && mikeX + mike.width < canvas.width) {
     mikeX += incrMike;
   } else if (isLeftArrow && mikeX > 0) {
     mikeX -= incrMike;
   }
-  if (isSpaceKey) {
-    ctx.drawImage(ball, mikeX + 12, ballY);
-    ballY -= incrBall;
-  }
-  
 }
+
+function draw() {
+    
+    ctx.drawImage(backImg, 0, 0);
+    ctx.drawImage(mike, mikeX, backImg.height - mike.height);
+      drawToby();
+      drawBall();
+      moveMike();
+      collision();
   
+  
+  }
+
 intervalID = setInterval(() => {
   requestAnimationFrame(draw);
-}, 5);
+}, 5)
 
 
 
 
+
+
+
+
+
+
+
+
+
+  // for (let i = 0; i < arrBall.length; i++) {
+    //   ctx.drawImage(ball, mikeX + 12, ballY)
+    //   ballY -= incrBall
+    //   if (ballY == 0) {
+    //     arrBall.push({
+    //       x: mikeX + 12,
+    //       y: ballY
+    //     });
+    //   }
+
+    // }
+    
+    // if (isSpaceKey) {
+    //   ctx.drawImage(ball, mikeX + 12, ballY);
+    //   ballY -= incrBall
+    
+    // }
